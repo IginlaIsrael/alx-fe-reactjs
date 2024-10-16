67d545ba57311c22a3643aeb02c171e481823b80
IginlaIsrael <iginlaisraelo@gmail.com>
2024-10-16 08:42:06 +0100

    adding multiple components

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,3 +1,10 @@
-<footer>
-  <p>© 2023 City Lovers</p>
-</footer>
+
+function footer() {
+    return(
+        <footer>
+        <p>© 2023 City Lovers</p>
+        </footer>
+    );
+    
+}
+export default footer;
