3a8bd31fe2860f8d34a26a4d1f2596215ee423cc
IginlaIsrael <iginlaisraelo@gmail.com>
2024-10-16 09:34:58 +0100

    duplicating and styling

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -40,6 +41,7 @@ function App() {
         <MainContent />
         <Footer />
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        <Counter />
       </div>
     </>
   )
