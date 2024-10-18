1c468350223c684396213d731610078313c1399b
IginlaIsrael <iginlaisraelo@gmail.com>
2024-10-18 14:12:35 +0100

    Advanced Routing in React with React Router

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,15 +1,23 @@
-// src/pages/Profile.jsx
 import React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
+import ProfileDetails from './ProfileDetails';
+import ProfileSettings from './ProfileSettings';
 
-const Profile = () => (
-  <div>
-    <h1>Profile Page</h1>
-    <ul>
-      <li><Link to="details">Profile Details</Link></li>
-      <li><Link to="settings">Profile Settings</Link></li>
-    </ul>
-    <Outlet />
-  </div>
-);
+const Profile = () => {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <ul>
+        <li><Link to="details">Profile Details</Link></li>
+        <li><Link to="settings">Profile Settings</Link></li>
+      </ul>
+
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
+    </div>
+  );
+};
+
 export default Profile;
