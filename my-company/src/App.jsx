a5818acd59be4b4605340bdc1c83bd375567a90f
IginlaIsrael <iginlaisraelo@gmail.com>
2024-10-20 06:01:24 +0100

    lol

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
